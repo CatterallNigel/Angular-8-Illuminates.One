41f3a898c96eb822926f1a88021c7d7b457fe81b
CatterallNigel <53537070+CatterallNigel@users.noreply.github.com>
2020-06-26 11:54:16 -0700

    Update data-actions-interface.ts

--- src/app/shared/modules/forms/interfaces/data-actions-interface.ts
+++ src/app/shared/modules/forms/interfaces/data-actions-interface.ts
@@ -1,8 +1,8 @@
 export interface IUserFormsActions {
   getStaticData(file: string): any;
-  postLogin(form: FormData, loginURL: string): Promise<any>;
-  postContactUs(form: FormData, contactURL: string): Promise<any>;
-  postRegister(form: FormData, registerURL: string): Promise<any>;
+  postLogin(form: FormData, loginURL: string): Promise<{id: string} | string>;
+  postContactUs(form: FormData, contactURL: string): Promise<string>;
+  postRegister(form: FormData, registerURL: string): Promise<string>;
 }
 
 export type IUserFormsActionsType = IUserFormsActions;
